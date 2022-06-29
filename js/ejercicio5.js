e2c549a4519e36866d1a0406ca43a0668b268ee4
Tomas <tomibudeguer20@gmail.com>
2022-06-29 00:13:41 -0300

    hice el ejercicio 5

---
+++ js/ejercicio5.js
@@ -0,0 +1,4 @@
+// Ejercicio 5
+var num1 = parseInt(prompt("Escriba el numero 1: "));
+var num2 = parseInt(prompt("Escriba el numero 2: "));
+document.write("<br>El resultado de la suma es: "+ (num1 + num2));
