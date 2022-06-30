32f06ef2caace34b98da918bd9d96965e4b0319f
Tomas <tomibudeguer20@gmail.com>
2022-06-29 23:57:01 -0300

    cambie el nombre de los parseInt

--- js/ejercicio5.js
+++ js/ejercicio5.js
@@ -1,4 +1,4 @@
 // Ejercicio 5
-var num1 = parseInt(prompt("Escriba el numero 1: "));
-var num2 = parseInt(prompt("Escriba el numero 2: "));
+var num1 = parseInt(prompt("Escriba un numero : "));
+var num2 = parseInt(prompt("Escriba otro numero : "));
 document.write("<br>El resultado de la suma es: "+ (num1 + num2));
