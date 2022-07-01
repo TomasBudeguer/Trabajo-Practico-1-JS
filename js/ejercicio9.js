a86ca19f18c13490ab9a8a9e8587fcb04184614e
Tomas <tomibudeguer20@gmail.com>
2022-07-01 14:09:09 -0300

    hice el ejercicio 9

---
+++ js/ejercicio9.js
@@ -0,0 +1,8 @@
+// ejercicio 9
+let frase = prompt('Escriba su frase: ')
+
+for(i = 0; i < frase.length; i++){
+    if( frase.charAt(i,frase.length)=== 'a' || frase.charAt(i,frase.length)=== 'e' || frase.charAt(i,frase.length)=== 'i' || frase.charAt(i,frase.length)=== 'o' || frase.charAt(i,frase.length)=== 'u'){
+        document.write(frase.charAt(i,frase.length))
+    }
+}
