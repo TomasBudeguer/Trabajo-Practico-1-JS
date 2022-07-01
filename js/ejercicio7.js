72dc0495ec837be411724f6aabf0b3570adfc582
Tomas <tomibudeguer20@gmail.com>
2022-07-01 00:47:39 -0300

    hice el ejercicio 7

--- js/ejercicio7.js
+++ js/ejercicio7.js
@@ -1,9 +1,11 @@
-// ejercicio 6
 let num1 = parseInt(prompt('ingrese un numero'))
 let num2 = parseInt(prompt('ingrese otro numero'))
+let num3 = parseInt(prompt('ingrese otro numero'))
 
-if(num1 > num2){
+if(num1 > num2 && num1 > num3){
     document.write('<p>El numero: '+ num1 + ' es mayor </p>')
-}else{
+}else if(num2 > num1 && num2 > num3){
     document.write('<p>El numero: '+ num2 + ' es mayor </p>')
+}else if(num3 > num1 && num3 > num2){
+    document.write('<p>El numero: '+ num3 + ' es mayor </p>')
 }
