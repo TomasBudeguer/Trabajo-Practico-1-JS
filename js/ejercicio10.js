b6d35f044042427654177c5bd2ea25dec0193580
Tomas <tomibudeguer20@gmail.com>
2022-07-01 11:26:07 -0300

    hice el ejercicio 10

---
+++ js/ejercicio10.js
@@ -0,0 +1,13 @@
+let num1 = parseInt(prompt('ingrese un numero'))
+
+if(num1 % 2 === 0 ){
+    document.write('<p>El numero: '+ num1 + ' es divisible por 2 </p>')
+}else if(num1 % 3 === 0 ){
+    document.write('<p>El numero: '+ num1 + ' es divisible por 3 </p>')
+}else if( num1 % 5 === 0 ){
+    document.write('<p>El numero: '+ num1 + ' es divisible por 5 </p>')
+}else if( num1 % 7 === 0 ){
+    document.write('<p>El numero: '+ num1 + ' es divisible por 7 </p>')
+}else{
+    document.write('<p>El numero: '+ num1 + ' NO divisible por 2, 3, 5 ni 7 </p>')
+}
