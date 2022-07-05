f33ba4b59c3394b184fda1e8c7f38c44cb2f8898
Tomas <tomibudeguer20@gmail.com>
2022-07-05 19:05:25 -0300

    agregue la forma del ejercicio 9 vista en clase

--- js/ejercicio9.js
+++ js/ejercicio9.js
@@ -1,5 +1,6 @@
 // ejercicio 9
 let frase = prompt('Escriba su frase: ')
+frase = frase.toLowerCase()
 
 for(i = 0; i < frase.length; i++){
     if( frase.charAt(i,frase.length)=== 'a' || frase.charAt(i,frase.length)=== 'e' || frase.charAt(i,frase.length)=== 'i' || frase.charAt(i,frase.length)=== 'o' || frase.charAt(i,frase.length)=== 'u'){
